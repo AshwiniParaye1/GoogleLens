8031ededb36377684dbe2d67fcb4f698c4c2f368
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-04-15 15:45:56 +0530

    update icons

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,20 +1,27 @@
-
-import { Home, Moon, Bell, Menu } from "lucide-react";
+import { Menu } from "lucide-react";
+import { AiOutlineBell } from "react-icons/ai";
+import { BsClockHistory } from "react-icons/bs";
+import { GoHomeFill } from "react-icons/go";
 import { Link, useLocation } from "react-router-dom";
 
 const BottomNav = () => {
   const location = useLocation();
-  
+
   return (
-    <nav className="nav-tabs">
-      <Link to="/" className={`google-tab ${location.pathname === '/' ? 'text-google-blue' : ''}`}>
-        <Home size={24} />
+    <nav className="nav-tabs p-2">
+      <Link
+        to="/"
+        className={`google-tab ${
+          location.pathname === "/" ? "text-google-blue" : ""
+        }`}
+      >
+        <GoHomeFill size={24} />
       </Link>
       <div className="google-tab">
-        <Moon size={24} />
+        <BsClockHistory size={20} />
       </div>
       <div className="google-tab">
-        <Bell size={24} />
+        <AiOutlineBell size={24} />
       </div>
       <div className="google-tab">
         <Menu size={24} />
