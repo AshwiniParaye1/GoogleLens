8b0be872746fd77ffb3c06125a6117501d897637
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-04-15 22:43:17 +0530

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import AccountMenu from "./AccountMenu";
 
 const GoogleSearchButton = () => (
   <div className="bg-google-card rounded-md text-white px-2 py-2 flex items-center gap-2">
-    <div className="bg-gray-900 rounded-md flex flex-row p-2">
+    <div className="bg-gray-900 rounded-md flex flex-row px-2 py-1.5 mr-1">
       <div className="mr-2 self-center">
         <img src="/google.png" width={18} height={18} alt="star" />
       </div>
