7f3547ba411c6b628e8a4b2622256bf146df0567
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-04-15 15:20:43 +0530

    fix widgets

--- src/components/WeatherWidget.tsx
+++ src/components/WeatherWidget.tsx
@@ -1,14 +1,13 @@
-
-import { Moon } from "lucide-react";
+import { IoMdMoon } from "react-icons/io";
 
 const WeatherWidget = () => {
   return (
     <div className="google-card flex justify-between items-center">
       <div>
-        <div className="text-sm text-gray-300 mb-1">Gurugram</div>
-        <div className="text-2xl font-semibold">30°</div>
+        <div className="text-sm font-medium text-gray-300 mb-2">Gurugram</div>
+        <div className="text-xl font-medium">30°</div>
       </div>
-      <Moon size={32} className="text-gray-300" />
+      <IoMdMoon size={28} className="text-gray-300 self-end" />
     </div>
   );
 };
