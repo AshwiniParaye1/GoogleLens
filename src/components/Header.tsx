c026b578a173971d1248ed17f6c8bc0b690f461e
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-04-15 20:44:09 +0530

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,17 @@
 //src/components/Header.tsx
 
-import { FlaskConical, Star } from "lucide-react";
+import { PiFlaskFill } from "react-icons/pi";
 import AccountMenu from "./AccountMenu";
 
 const GoogleSearchButton = () => (
-  <div className="bg-google-card text-white rounded-full px-4 py-2 flex items-center gap-2">
-    <div className="bg-[#4285F4] text-white rounded-full w-5 h-5 flex items-center justify-center mr-1 text-xs font-bold">
-      G
+  <div className="bg-google-card rounded-md text-white px-2 py-2 flex items-center gap-2">
+    <div className="bg-gray-900 rounded-md flex flex-row p-2">
+      <div className="mr-2 self-center">
+        <img src="/public/google.png" width={18} height={18} alt="star" />
+      </div>
+      Search
     </div>
-    Search
-    <Star className="text-google-yellow" size={16} />
+    <img src="/public/star.png" width={18} height={18} alt="star" />
   </div>
 );
 
@@ -26,7 +28,7 @@ const Header = ({
       {variant === "home" && (
         <>
           <div className="flex items-center justify-center w-10 h-10">
-            <FlaskConical className="text-blue-400" size={28} />
+            <PiFlaskFill className="text-blue-400" size={28} />
           </div>
 
           <GoogleSearchButton />
